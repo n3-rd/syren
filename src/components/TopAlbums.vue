ba66426f21879d11b0566fa339ca8fbae11db555
n3-rd <godwinjdayo@gmail.com>
2021-01-26 15:35:00 +0100

    desktop album popup

--- src/components/TopAlbums.vue
+++ src/components/TopAlbums.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
-    <!-- top albums -->
+
+
+    <!--mobile top albums -->
     <h5 class="q-my-sm q-pl-md q-mt-xl">Top Albums</h5>
 
-    <q-list>
+    <q-list class="lt-md">
       <q-item v-for="album in topAlbumsData" :key="album.title">
 
         <q-popup-proxy>
@@ -53,6 +55,83 @@
 
       <q-separator spaced inset="item" />
     </q-list>
+
+
+    <!-- desktop top albums -->
+
+
+
+<div class="gt-sm q-px-lg">
+        <div class="row">
+
+          <div
+            class="col-4 q-px-md q-pb-xl"
+             v-for="album in topAlbumsData"
+              :key="album.title"
+          >
+            <q-popup-proxy>
+    <div class="gt-sm">
+      <q-card
+        class="bg-dark text-white fixed-top-right"
+        style="width: 30%; z-index: 999"
+      >
+        <q-bar> </q-bar>
+        <q-scroll-area style="height: 100vh">
+          <q-card-section>
+            <div class="q-my-lg">
+              <div class="text-h6 text-center">{{album.title}}</div>
+              <div class="text-h6 text-weight-light text-center">
+                {{ album.artist.name }}
+              </div>
+            </div>
+            <div>
+                         <q-img :src="album.cover_big"/>
+
+               
+            </div>
+          </q-card-section>
+        </q-scroll-area>
+      </q-card>
+    </div>
+            </q-popup-proxy>
+            <q-img
+              :src="album.cover_big"
+              :img-style="{ borderRadius: '15px' }"
+              spinner-color="white"
+              class="rounded cursor-pointer"
+              width="100%"
+              v-ripple
+            >
+              <!-- error if image fails to load -->
+              <template v-slot:error>
+                <div
+                  class="absolute-full flex flex-center bg-negative text-white"
+                >
+                  Cannot load image
+                </div>
+              </template>
+              <!-- music indicator overlay -->
+              <div v-show="album.explicit_lyrics">
+                <div
+                  class="absolute-top-left text-weight-bold text-center relative-position"
+                >
+                  E<q-tooltip>May contain explicit lyrics</q-tooltip>
+                </div>
+              </div>
+              <!-- track details overlay -->
+              <div class="absolute-bottom text-subtitle1 text-center">
+                {{ album.artist.name }}
+                <div class="text-weight-light">{{ album.title }}</div>
+              </div>
+            </q-img>
+          </div>
+        </div>
+      </div>
+
+
+
+
+
   </div>
 </template>
 
